405ca550b28fd403426dee8bd343755b620af834
imeneHamida <imene.hamida18@gmail.com>
2023-11-02 15:42:44 -0700

    some errs fixed

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -18,7 +18,7 @@ int _putchar(char c)
 return write(1, &c, 1);
 }
 
-void print_number(long long num)
+void print_number(long num)
 {
 if (num < 0)
 {
@@ -34,22 +34,26 @@ _putchar(num % 10 + '0');
 
 int main(int argc, char *argv[])
 {
+char *num1;
+char *num2;
+long result;
+
 if (argc != 3)
 {
 write(1, "Error\n", 6);
-return (98);
+return 98;
 }
 
-char *num1 = argv[1];
-char *num2 = argv[2];
+num1 = argv[1];
+num2 = argv[2];
 
 if (!is_positive_number(num1) || !is_positive_number(num2))
 {
 write(1, "Error\n", 6);
-return (98);
+return 98;
 }
 
-long long result = atoll(num1) * atoll(num2);
+result = atol(num1) * atol(num2);
 print_number(result);
 _putchar('\n');
 
