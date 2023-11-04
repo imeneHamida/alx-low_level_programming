b1ff8de65c4ffdc3596623a61a57179dbd0f47f4
imeneHamida <imene.hamida18@gmail.com>
2023-11-04 11:39:18 -0700

    some errors fixed

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,8 +4,15 @@
  * main - Entry point of the program
  * @argc: The number of command-line arguments
  * @argv: An array containing the arguments
+ *
+ * Description: This function prints the name of
+ * the program, including its path.
+ * The `argc` parameter is not used in this program,
+ * and `argv[0]` contains the program name.
+ *
+ * Return: 0 (Success)
  */
-int main(int argc __attribute__((unused)), char *argv[] __attribute__((unused)))
+int main(int argc __attribute__((unused)), char *argv[])
 {
 printf("%s\n", argv[0]);
 return (0);
