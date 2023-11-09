fdbd3caf8aa13d5429f2b42809cd64af660e358f
imeneHamida <imene.hamida18@gmail.com>
2023-11-09 15:58:42 -0800

    Variadic functions tasks done

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,35 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_strings - Prints strings,
+ * separated by a specified string.
+ * @separator: The string to be printed between the strings.
+ * @n: The number of strings to print.
+ * @...: Variable number of strings to be printed.
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+va_list args;
+unsigned int i;
+char *str;
+
+va_start(args, n);
+
+for (i = 0; i < n; i++)
+{
+str = va_arg(args, char *);
+if (str == NULL)
+printf("(nil)");
+else
+printf("%s", str);
+
+if (i < n - 1 && separator != NULL)
+printf("%s", separator);
+}
+
+va_end(args);
+
+printf("\n");
+}
---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,52 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_all - Prints values of different
+ * types based on the format.
+ * @format: A string specifying the types
+ * of arguments to print.
+ * @...: Variable number of arguments
+ * of different types.
+ */
+void print_all(const char * const format, ...)
+{
+va_list args;
+unsigned int i = 0;
+char *separator = "";
+char *str;
+
+va_start(args, format);
+
+while (format && format[i])
+{
+switch (format[i])
+{
+case 'c':
+printf("%s%c", separator, va_arg(args, int));
+break;
+case 'i':
+printf("%s%d", separator, va_arg(args, int));
+break;
+case 'f':
+printf("%s%f", separator, va_arg(args, double));
+break;
+case 's':
+str = va_arg(args, char *);
+if (str == NULL)
+str = "(nil)";
+printf("%s%s", separator, str);
+break;
+default:
+i++;
+continue;
+}
+separator = ", ";
+i++;
+}
+
+va_end(args);
+
+printf("\n");
+}
