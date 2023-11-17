8bcc7213a7c54acce991f1887bb7267907ca1b3f
imeneHamida <imene.hamida18@gmail.com>
2023-11-17 08:24:45 -0800

    linked lists mandatory tasks done

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,6 +2,8 @@
 #define LISTS_H
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
 
 /**
  * struct list_s - singly linked list
@@ -18,8 +20,11 @@ typedef struct list_s
     struct list_s *next;
 } list_t;
 
-/* Function prototype */
 size_t print_list(const list_t *h);
 int _putchar(char c);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
 #endif
