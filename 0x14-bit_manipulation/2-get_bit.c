3117dcd8af5852a0902acdaa5cc69a48b7aa3d76
imeneHamida <imene.hamida18@gmail.com>
2023-11-23 15:47:19 -0800

    bit manipulation tasks fixed

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -6,7 +6,7 @@
  * @index: The index, starting from 0, of the bit to get.
  *
  * Return: The value of the bit at the specified index,
- * 	or -1 if an error occurs.
+ * or -1 if an error occurs.
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
